10d1ea3b263349c46ee55da99687e14d43d7aaec
mert1745 <mertkose1745@gmail.com>
2024-04-13 12:13:22 +0300

    Initial commit

---
+++ src/main/java/com/ogz/magazine/WebMagazineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ogz.magazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMagazineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMagazineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ogz/magazine/WebMagazineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogz.magazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMagazineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

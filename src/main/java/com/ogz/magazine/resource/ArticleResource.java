64e4158ae7028609b62f0265f8b4fba3b0530178
mert1745 <mertkose1745@gmail.com>
2024-04-13 18:17:09 +0300

    Remove endpoint prefix

--- src/main/java/com/ogz/magazine/resource/ArticleResource.java
+++ src/main/java/com/ogz/magazine/resource/ArticleResource.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-//TODO mkose api prefix should be defined globally
-@RestController("api/v1/articles")
+@RestController("/articles")
 public class ArticleResource {
     private final ArticleService articleService;
 
